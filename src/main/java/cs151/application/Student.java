46fa7a503dcf2f02be92e953a50abbb2cee3b041
spinningjoel <kobedeng00@gmail.com>
2025-10-17 22:23:54 -0700

    added view student profiles

---
+++ src/main/java/cs151/application/Student.java
@@ -0,0 +1,103 @@
+package cs151.application;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+
+public class Student implements Comparable<Student>{
+    @FXML
+    private String full_Name;
+
+    @FXML
+    private String academic_Status;
+
+    @FXML
+    private String current_Job_Status;
+
+    @FXML
+    private String job_Details;
+
+    @FXML
+    private String programming_Languages;
+
+    @FXML
+    private String databases;
+
+    @FXML
+    private String preferred_Role;
+
+    @FXML
+    private String comments;
+
+    @FXML
+    private BooleanProperty whitelist;
+
+    @FXML
+    private BooleanProperty blacklist;
+
+    public BooleanProperty whitelistProperty() {
+        return this.whitelist;
+    }
+
+    public Boolean isWhitelist() {
+        return this.whitelist.get();
+    }
+
+    public BooleanProperty blacklistProperty() {
+        return this.blacklist;
+    }
+
+    public Boolean isBlacklist() {
+        return this.blacklist.get();
+    }
+
+    public String getComments() {
+        return this.comments;
+    }
+
+    public String getPreferred_Role() {
+        return this.preferred_Role;
+    }
+
+    public String getDatabases() {
+        return this.databases;
+    }
+
+    public String getProgramming_Languages() {
+        return this.programming_Languages;
+    }
+
+    public String getJob_Details() {
+        return this.job_Details;
+    }
+
+    public String getCurrent_Job_Status() {
+        return this.current_Job_Status;
+    }
+
+    public String getAcademic_Status() {
+        return this.academic_Status;
+    }
+
+    public String getFull_Name() {
+        return this.full_Name;
+    }
+
+    public Student(String full_Name, String academic_Status, String current_Job_Status, String job_Details, String programming_Languages, String databases, String preferred_Role, String comments, Boolean whitelist, Boolean blacklist) {
+        this.full_Name = full_Name;
+        this.academic_Status = academic_Status;
+        this.current_Job_Status = current_Job_Status;
+        this.job_Details = job_Details;
+        this.programming_Languages = programming_Languages;
+        this.databases = databases;
+        this.preferred_Role = preferred_Role;
+        this.comments = comments;
+        this.whitelist = new SimpleBooleanProperty(whitelist);
+        this.blacklist = new SimpleBooleanProperty(blacklist);
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.full_Name.compareTo(o.full_Name);
+    }
+}
