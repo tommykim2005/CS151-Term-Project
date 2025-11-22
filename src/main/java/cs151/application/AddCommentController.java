abe76fd2c3fcbd7d7bb2cd280ff325abde0d9b41
spinningjoel <kobedeng00@gmail.com>
2025-11-21 16:13:05 -0800

    a

--- src/main/java/cs151/application/AddCommentController.java
+++ src/main/java/cs151/application/AddCommentController.java
@@ -151,39 +151,5 @@ public class AddCommentController {
         items.add(c);
     }
 
-    public class comment {
-        private final StringProperty date;
-        private final StringProperty comment;
-
-        public comment(String date, String comment) {
-            this.date = new SimpleStringProperty(date);
-            this.comment = new SimpleStringProperty(comment);
-        }
-
-        public String getDate() {
-            return date.get();
-        }
-
-        public void setDate(String date) {
-            this.date.set(date);
-        }
-
-        public StringProperty dateProperty() {
-            return date;
-        }
-
-        public String getComment() {
-            return comment.get();
-        }
-
-        public void setComment(String comment) {
-            this.comment.set(comment);
-        }
-
-        public StringProperty commentProperty() {
-            return comment;
-        }
-    }
-
 
 }
